e8fc93edb8a3610299717dd6ec51aa47152e4b75
Peter Iskandar <piskandar@pivotal.io>
2018-01-16 17:05:10 -0500

    updated pipeline

--- module/ci/tasks/build/build.sh
+++ module/ci/tasks/build/build.sh
@@ -6,11 +6,10 @@ echo $CHECKSUM_BASE64
 echo -n $BASE64_FILE_DATA > file_base64.file
 #echo `shasum file_base64.file | awk '{print $1}'`
 
-echo `openssl dgst -sha1 concourse-release/module/files/testfile`
 echo `openssl dgst -sha1 file_base64.file`
 
-openssl base64 -A -in concourse-release/module/files/testfile -out testbase64.file
+#openssl base64 -A -in concourse-release/module/files/testfile -out testbase64.file
 
 #echo `shasum file.file | awk '{print $1}'`
 #echo `shasum testbase64.file`
-echo `openssl dgst -sha1 testbase64.file`
+#echo `openssl dgst -sha1 testbase64.file`
