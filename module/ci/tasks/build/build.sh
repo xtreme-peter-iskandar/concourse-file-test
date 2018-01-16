ea610f553276969762c454b2b3d64d935006bc66
Peter Iskandar <piskandar@pivotal.io>
2018-01-16 16:55:44 -0500

    updated pipeline

--- module/ci/tasks/build/build.sh
+++ module/ci/tasks/build/build.sh
@@ -3,7 +3,7 @@
 echo $CHECKSUM
 echo $CHECKSUM_BASE64
 #echo $BASE64_FILE_DATA
-echo $BASE64_FILE_DATA > file_base64.file
+echo -n $BASE64_FILE_DATA > file_base64.file
 #echo `shasum file_base64.file | awk '{print $1}'`
 
 echo `wc -c file_base64.file`
