7132b74964be485fb7cd5357840909a7e90d46cc
Peter Iskandar <piskandar@pivotal.io>
2018-01-17 11:10:34 -0500

    updated pipeline

--- module/ci/tasks/build/build.sh
+++ module/ci/tasks/build/build.sh
@@ -2,15 +2,17 @@
 
 echo $CHECKSUM
 echo $CHECKSUM_BASE64
-#echo $BASE64_FILE_DATA
 echo -n $BASE64_FILE_DATA > file_base64.file
-#echo `shasum file_base64.file | awk '{print $1}'`
+
 
 echo `openssl dgst -sha1 file_base64.file`
 
 openssl base64 -A -d -in file_base64.file -out file.file
 
 echo `openssl dgst -sha1 file.file`
-#echo `shasum file.file | awk '{print $1}'`
-#echo `shasum testbase64.file`
-#echo `openssl dgst -sha1 testbase64.file`
+
+KEYSTORE_FILE_PATH=file.file
+
+cd test
+
+./gradle printEnv
