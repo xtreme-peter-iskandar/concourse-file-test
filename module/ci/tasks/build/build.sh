e7a6450f449d3202971288806cafbcaec5389997
Peter Iskandar <piskandar@pivotal.io>
2018-01-17 11:28:31 -0500

    updated pipeline

--- module/ci/tasks/build/build.sh
+++ module/ci/tasks/build/build.sh
@@ -11,10 +11,19 @@ openssl base64 -A -d -in file_base64.file -out file.file
 
 echo `openssl dgst -sha1 file.file`
 
-export KEYSTORE_FILE_PATH=file.file
+export KEYSTORE_FILE_PATH=keystore-file-path
+export CERTS_FILE_PATH=certs-file-path
+mkdir keystore-file-path
+mkdir certs-file-path
+
+
+touch keystore-file-path/test.jks
+touch certs-file-path/samlKeystore.jks
 
 cd concourse-release/test
 pwd
 ls -la
 echo $KEYSTORE_FILE_PATH
 ./gradlew printEnv
+
+ls -la
